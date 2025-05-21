8b4ec5ed80726adfc0985badc7a170f920dc2d83
João Henrique <jhenrmm@gmail.com>
2025-05-21 12:49:45 -0300

    Created a API RESTful and learned SpringBoot

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,46 @@
+package com.example.springboot.models;
+
+
+import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name= "TB_PRODUCTS")
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID idProject;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(UUID idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+}
